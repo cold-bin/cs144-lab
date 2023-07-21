c6b7293d7286a7aa89b004764c90c9a5d21606da
刘海斌 <3398341353@qq.com>
2023-07-21 13:24:31 +0800

    feat: 完成lab1代码编写

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -2,17 +2,42 @@
 
 using namespace std;
 
-void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring, Writer& output )
-{
-  // Your code here.
-  (void)first_index;
-  (void)data;
-  (void)is_last_substring;
-  (void)output;
+void Reassembler::insert(uint64_t first_index, string data, bool is_last_substring, Writer &output) {
+    // Your code here.
+
+    // over the left `cap`,do nothing and throw data aside
+    if (first_index >= cap_ || data.size() > cap_) {
+        return;
+    }
+
+    // maybe the storage is not enough, send and free them
+    if (data.size() + bytes_pending() > cap_) {
+        for (uint64_t i = 0; i < inter_storage_.size(); ++i) {
+            // 出现失序的数据了，不能push到bytestream里
+            if (inter_storage_.at(i).empty()) { break; }
+            // 没有乱序时，继续push
+            output.push(inter_storage_[i]);
+            inter_storage_[i].clear();
+            available_cap_--;
+        }
+    }
+
+    // overlapping
+    if (!inter_storage_[first_index].empty()) {
+        return;
+    }
+
+    inter_storage_[first_index] = data;
+    available_cap_ += data.length();
+
+    if (is_last_substring) {
+        output.close();
+    }
 }
 
-uint64_t Reassembler::bytes_pending() const
-{
-  // Your code here.
-  return {};
+uint64_t Reassembler::bytes_pending() const {
+    // Your code here.
+    return available_cap_;
 }
+
+Reassembler::Reassembler(uint64_t cap) : cap_(cap), inter_storage_(cap) {}
--- src/reassembler.hh
+++ src/reassembler.hh
@@ -2,33 +2,38 @@
 
 #include "byte_stream.hh"
 
-#include <string>
+class Reassembler {
+private:
+    uint64_t cap_; // Reassembler 的内部存储最大容量
+    std::vector<std::string> inter_storage_; // 实现优先队列
+    uint64_t available_cap_{0};
 
-class Reassembler
-{
 public:
-  /*
-   * Insert a new substring to be reassembled into a ByteStream.
-   *   `first_index`: the index of the first byte of the substring
-   *   `data`: the substring itself
-   *   `is_last_substring`: this substring represents the end of the stream
-   *   `output`: a mutable reference to the Writer
-   *
-   * The Reassembler's job is to reassemble the indexed substrings (possibly out-of-order
-   * and possibly overlapping) back into the original ByteStream. As soon as the Reassembler
-   * learns the next byte in the stream, it should write it to the output.
-   *
-   * If the Reassembler learns about bytes that fit within the stream's available capacity
-   * but can't yet be written (because earlier bytes remain unknown), it should store them
-   * internally until the gaps are filled in.
-   *
-   * The Reassembler should discard any bytes that lie beyond the stream's available capacity
-   * (i.e., bytes that couldn't be written even if earlier gaps get filled in).
-   *
-   * The Reassembler should close the stream after writing the last byte.
-   */
-  void insert( uint64_t first_index, std::string data, bool is_last_substring, Writer& output );
+    /*
+     * Insert a new substring to be reassembled into a ByteStream.
+     *   `first_index`: the index of the first byte of the substring
+     *   `data`: the substring itself
+     *   `is_last_substring`: this substring represents the end of the stream
+     *   `output`: a mutable reference to the Writer
+     *
+     * The Reassembler's job is to reassemble the indexed substrings (possibly out-of-order
+     * and possibly overlapping) back into the original ByteStream. As soon as the Reassembler
+     * learns the next byte in the stream, it should write it to the output.
+     *
+     * If the Reassembler learns about bytes that fit within the stream's available capacity
+     * but can't yet be written (because earlier bytes remain unknown), it should store them
+     * internally until the gaps are filled in.
+     *
+     * The Reassembler should discard any bytes that lie beyond the stream's available capacity
+     * (i.e., bytes that couldn't be written even if earlier gaps get filled in).
+     *
+     * The Reassembler should close the stream after writing the last byte.
+     */
+    void insert(uint64_t first_index, std::string data, bool is_last_substring, Writer &output);
 
-  // How many bytes are stored in the Reassembler itself?
-  uint64_t bytes_pending() const;
+    // How many bytes are stored in the Reassembler itself?
+    uint64_t bytes_pending() const;
+
+    // construct function
+    Reassembler(uint64_t cap);
 };
