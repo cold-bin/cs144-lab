b86eeed8a596570d14da855bac73d8ac66168666
刘海斌 <3398341353@qq.com>
2023-07-28 17:06:37 +0800

    feat: 新增lab4的代码实现

--- src/network_interface.cc
+++ src/network_interface.cc
@@ -2,16 +2,16 @@
 
 #include "arp_message.hh"
 #include "ethernet_frame.hh"
+#include "../util/socket.hh"
 
 using namespace std;
 
 // ethernet_address: Ethernet (what ARP calls "hardware") address of the interface
 // ip_address: IP (what ARP calls "protocol") address of the interface
-NetworkInterface::NetworkInterface( const EthernetAddress& ethernet_address, const Address& ip_address )
-  : ethernet_address_( ethernet_address ), ip_address_( ip_address )
-{
-  cerr << "DEBUG: Network interface has Ethernet address " << to_string( ethernet_address_ ) << " and IP address "
-       << ip_address.ip() << "\n";
+NetworkInterface::NetworkInterface(const EthernetAddress &ethernet_address, const Address &ip_address)
+        : ethernet_address_(ethernet_address), ip_address_(ip_address) {
+    cerr << "DEBUG: Network interface has Ethernet address " << to_string(ethernet_address_) << " and IP address "
+         << ip_address.ip() << "\n";
 }
 
 // dgram: the IPv4 datagram to be sent
@@ -20,26 +20,157 @@ NetworkInterface::NetworkInterface( const EthernetAddress& ethernet_address, con
 
 // Note: the Address type can be converted to a uint32_t (raw 32-bit IP address) by using the
 // Address::ipv4_numeric() method.
-void NetworkInterface::send_datagram( const InternetDatagram& dgram, const Address& next_hop )
-{
-  (void)dgram;
-  (void)next_hop;
+void NetworkInterface::send_datagram(const InternetDatagram &dgram, const Address &next_hop) {
+    uint32_t const next_hop_ipv4_num = next_hop.ipv4_numeric();
+    auto iter1 = arp_table_.find(next_hop_ipv4_num);
+    if (iter1 != arp_table_.end() && iter1->second.ttl > 0) { /* 缓存命中 */
+        EthernetFrame ef;
+        ef.header.type = EthernetHeader::TYPE_IPv4;
+        ef.header.src = NetworkInterface::ethernet_address_;
+        ef.header.dst = iter1->second.eth_addr;
+        ef.payload = serialize(dgram);
+        outbound_frames_.push(ef);
+        return;
+    }
+
+    // 发送arp request
+    auto iter2 = arp_requests_lifetime_.find(next_hop_ipv4_num);
+    if (iter2 != arp_requests_lifetime_.end() && iter2->second > 0) {/* arp请求已经发送，但是还没有到5seconds，等待接收arp响应 */
+        return;
+    }
+
+    EthernetFrame ef;
+    ef.header.type = EthernetHeader::TYPE_ARP;
+    ef.header.src = NetworkInterface::ethernet_address_;
+    ef.header.dst = ETHERNET_BROADCAST;
+
+    ARPMessage arp_msg;
+    arp_msg.opcode = ARPMessage::OPCODE_REQUEST;
+    arp_msg.sender_ip_address = NetworkInterface::ip_address_.ipv4_numeric();
+    arp_msg.sender_ethernet_address = NetworkInterface::ethernet_address_;
+    arp_msg.target_ip_address = next_hop_ipv4_num;
+    arp_msg.target_ethernet_address = {0, 0, 0, 0, 0, 0};
+
+    ef.payload = serialize(arp_msg);
+    outbound_frames_.push(ef);
+
+    // 缓存arp 请求时间
+    arp_requests_waiting_list_.push_back(std::pair<Address, InternetDatagram>(next_hop, dgram));
+    arp_requests_lifetime_.emplace(std::pair<uint32_t, size_t>(next_hop_ipv4_num, ARP_REQUEST_DEFAULT_TTL));
 }
 
 // frame: the incoming Ethernet frame
-optional<InternetDatagram> NetworkInterface::recv_frame( const EthernetFrame& frame )
-{
-  (void)frame;
-  return {};
+optional<InternetDatagram> NetworkInterface::recv_frame(const EthernetFrame &frame) {
+    // 不接收非广播地址或目标mac为自身mac地址的的mac帧
+    if (frame.header.dst != NetworkInterface::ethernet_address_ || frame.header.dst != ETHERNET_BROADCAST) {
+        return nullopt;
+    }
+
+    if (frame.header.type == EthernetHeader::TYPE_IPv4) {
+        InternetDatagram ip_dgram;
+        auto ok = parse(ip_dgram, frame.payload);
+        if (ok) {
+            return ip_dgram;
+        }
+
+        return nullopt;
+    }
+
+    if (frame.header.type == EthernetHeader::TYPE_ARP) {
+        ARPMessage arp_msg;
+        auto ok = parse(arp_msg, frame.payload);
+        if (ok) {
+
+            const bool is_arp_request =
+                    arp_msg.opcode == ARPMessage::OPCODE_REQUEST &&
+                    arp_msg.target_ip_address == ip_address_.ipv4_numeric();
+
+            if (is_arp_request) {
+                ARPMessage arp_reply_msg;
+                arp_reply_msg.opcode = ARPMessage::OPCODE_REPLY;
+                arp_reply_msg.sender_ip_address = ip_address_.ipv4_numeric();
+                arp_reply_msg.sender_ethernet_address = NetworkInterface::ethernet_address_;
+                arp_reply_msg.target_ip_address = arp_msg.sender_ip_address;
+                arp_reply_msg.target_ethernet_address = arp_msg.sender_ethernet_address;
+
+                EthernetFrame arp_reply_eth_frame;
+                arp_reply_eth_frame.header.src = NetworkInterface::ethernet_address_;
+                arp_reply_eth_frame.header.dst = arp_msg.sender_ethernet_address;
+                arp_reply_eth_frame.header.type = EthernetHeader::TYPE_ARP;
+                arp_reply_eth_frame.payload = serialize(arp_reply_msg);
+                outbound_frames_.push(arp_reply_eth_frame);
+            }
+
+            const bool is_arp_response =
+                    arp_msg.opcode == ARPMessage::OPCODE_REPLY && arp_msg.target_ethernet_address == ethernet_address_;
+
+            // we can get arp info from either ARP request or ARP reply
+            if (is_arp_request || is_arp_response) {
+                arp_table_.emplace(std::pair(arp_msg.sender_ip_address,
+                                             arp_t{arp_msg.sender_ethernet_address, ARP_DEFAULT_TTL}));
+
+                // delete arp datagrams waiting list
+                for (auto iter = arp_requests_waiting_list_.begin(); iter != arp_requests_waiting_list_.end();) {
+                    const auto &[ipv4_addr, datagram] = *iter;
+                    if (ipv4_addr.ipv4_numeric() == arp_msg.sender_ip_address) {
+                        send_datagram(datagram, ipv4_addr);
+                        iter = arp_requests_waiting_list_.erase(iter);
+                    } else {
+                        iter++;
+                    }
+                }
+                arp_requests_lifetime_.erase(arp_msg.sender_ip_address);
+            }
+        }
+    }
+
+    return nullopt;
 }
 
 // ms_since_last_tick: the number of milliseconds since the last call to this method
-void NetworkInterface::tick( const size_t ms_since_last_tick )
-{
-  (void)ms_since_last_tick;
+void NetworkInterface::tick(const size_t ms_since_last_tick) {
+    // clear arp cache when arp cache is out of date
+    for (auto iter = arp_table_.begin(); iter != arp_table_.end();) {
+        if (iter->second.ttl <= ms_since_last_tick) {
+            iter = arp_table_.erase(iter);
+        } else {
+            iter->second.ttl -= ms_since_last_tick;
+            iter++;
+        }
+    }
+
+    // resend the arp request when it is time to resend. (over 5 seconds)
+    for (auto iter = arp_requests_waiting_list_.begin(); iter != arp_requests_waiting_list_.end(); iter++) {
+        auto key = iter->first.ipv4_numeric();
+        if (arp_requests_lifetime_[key] <= ms_since_last_tick) {
+
+            arp_requests_lifetime_[key] = ARP_REQUEST_DEFAULT_TTL;
+            // resend mac frame
+            EthernetFrame ef;
+            ef.header.dst = ETHERNET_BROADCAST;
+            ef.header.src = NetworkInterface::ethernet_address_;
+            ef.header.type = EthernetHeader::TYPE_ARP;
+
+            ARPMessage arp_msg;
+            arp_msg.opcode = ARPMessage::OPCODE_REQUEST;
+            arp_msg.sender_ip_address = NetworkInterface::ip_address_.ipv4_numeric();
+            arp_msg.sender_ethernet_address = NetworkInterface::ethernet_address_;
+            arp_msg.target_ip_address = key;
+            arp_msg.target_ethernet_address = {0, 0, 0, 0, 0, 0};
+
+            ef.payload = serialize(arp_msg);;
+            outbound_frames_.push(ef);
+            continue;
+        }
+        arp_requests_lifetime_[key] -= ms_since_last_tick;
+    }
 }
 
-optional<EthernetFrame> NetworkInterface::maybe_send()
-{
-  return {};
+optional<EthernetFrame> NetworkInterface::maybe_send() {
+    if (!outbound_frames_.empty()) {
+        auto res = outbound_frames_.front();
+        outbound_frames_.pop();
+        return res;
+    }
+    return nullopt;
 }
--- src/network_interface.hh
+++ src/network_interface.hh
@@ -10,6 +10,7 @@
 #include <queue>
 #include <unordered_map>
 #include <utility>
+#include <map>
 
 // A "network interface" that connects IP (the internet layer, or network layer)
 // with Ethernet (the network access layer, or link layer).
@@ -32,36 +33,57 @@
 // the network interface passes it up the stack. If it's an ARP
 // request or reply, the network interface processes the frame
 // and learns or replies as necessary.
-class NetworkInterface
-{
+class NetworkInterface {
 private:
-  // Ethernet (known as hardware, network-access, or link-layer) address of the interface
-  EthernetAddress ethernet_address_;
+    // Ethernet (known as hardware, network-access, or link-layer) address of the interface
+    EthernetAddress ethernet_address_;
 
-  // IP (known as Internet-layer or network-layer) address of the interface
-  Address ip_address_;
+    // IP (known as Internet-layer or network-layer) address of the interface
+    Address ip_address_;
+
+    // outbound Ethernet frames which will be sent by the Network Interface
+    std::queue<EthernetFrame> outbound_frames_{};
+
+    // ARP will be stored for 30s at most, which can reduce the length of ARP table,
+    // increasing the enquiry speed. What's more,
+    const size_t ARP_DEFAULT_TTL = static_cast<size_t>(30 * 1000);
+    const size_t ARP_REQUEST_DEFAULT_TTL = static_cast<size_t>(5 * 1000);
+
+    typedef struct arp {
+        EthernetAddress eth_addr; // mac address
+        size_t ttl; // time to live
+    } arp_t;
+
+    // ip-mac address cache
+    std::unordered_map<uint32_t/* target ipv4 numeric */, arp_t> arp_table_{};
+
+    // wait for arp request's reply
+    std::list<std::pair<Address, InternetDatagram>> arp_requests_waiting_list_{};
+
+    // ttl for every arp request to accept reply
+    std::unordered_map<uint32_t /* ipv4 numeric */, size_t /* ttl */> arp_requests_lifetime_{};
 
 public:
-  // Construct a network interface with given Ethernet (network-access-layer) and IP (internet-layer)
-  // addresses
-  NetworkInterface( const EthernetAddress& ethernet_address, const Address& ip_address );
+    // Construct a network interface with given Ethernet (network-access-layer) and IP (internet-layer)
+    // addresses
+    NetworkInterface(const EthernetAddress &ethernet_address, const Address &ip_address);
 
-  // Access queue of Ethernet frames awaiting transmission
-  std::optional<EthernetFrame> maybe_send();
+    // Access queue of Ethernet frames awaiting transmission
+    std::optional<EthernetFrame> maybe_send();
 
-  // Sends an IPv4 datagram, encapsulated in an Ethernet frame (if it knows the Ethernet destination
-  // address). Will need to use [ARP](\ref rfc::rfc826) to look up the Ethernet destination address
-  // for the next hop.
-  // ("Sending" is accomplished by making sure maybe_send() will release the frame when next called,
-  // but please consider the frame sent as soon as it is generated.)
-  void send_datagram( const InternetDatagram& dgram, const Address& next_hop );
+    // Sends an IPv4 datagram, encapsulated in an Ethernet frame (if it knows the Ethernet destination
+    // address). Will need to use [ARP](\ref rfc::rfc826) to look up the Ethernet destination address
+    // for the next hop.
+    // ("Sending" is accomplished by making sure maybe_send() will release the frame when next called,
+    // but please consider the frame sent as soon as it is generated.)
+    void send_datagram(const InternetDatagram &dgram, const Address &next_hop);
 
-  // Receives an Ethernet frame and responds appropriately.
-  // If type is IPv4, returns the datagram.
-  // If type is ARP request, learn a mapping from the "sender" fields, and send an ARP reply.
-  // If type is ARP reply, learn a mapping from the "sender" fields.
-  std::optional<InternetDatagram> recv_frame( const EthernetFrame& frame );
+    // Receives an Ethernet frame and responds appropriately.
+    // If type is IPv4, returns the datagram.
+    // If type is ARP request, learn a mapping from the "sender" fields, and send an ARP reply.
+    // If type is ARP reply, learn a mapping from the "sender" fields.
+    std::optional<InternetDatagram> recv_frame(const EthernetFrame &frame);
 
-  // Called periodically when time elapses
-  void tick( size_t ms_since_last_tick );
+    // Called periodically when time elapses
+    void tick(size_t ms_since_last_tick);
 };
