7928c01b0679f309125709672d73a0532b6df67d
刘海斌 <3398341353@qq.com>
2023-07-31 17:07:52 +0800

    feat: 完成lab5的代码实现

--- src/router.cc
+++ src/router.cc
@@ -11,19 +11,46 @@ using namespace std;
 // next_hop: The IP address of the next hop. Will be empty if the network is directly attached to the router (in
 //    which case, the next hop address should be the datagram's final destination).
 // interface_num: The index of the interface to send the datagram out on.
-void Router::add_route( const uint32_t route_prefix,
-                        const uint8_t prefix_length,
-                        const optional<Address> next_hop,
-                        const size_t interface_num )
-{
-  cerr << "DEBUG: adding route " << Address::from_ipv4_numeric( route_prefix ).ip() << "/"
-       << static_cast<int>( prefix_length ) << " => " << ( next_hop.has_value() ? next_hop->ip() : "(direct)" )
-       << " on interface " << interface_num << "\n";
-
-  (void)route_prefix;
-  (void)prefix_length;
-  (void)next_hop;
-  (void)interface_num;
+void Router::add_route(const uint32_t route_prefix, /* 目标ip */
+                       const uint8_t prefix_length, /* 子网掩码 */
+                       const optional<Address> next_hop, /* 如果不是直连路由器，那么依然需要next hop，否则直接走路由器网口出去就是目的地址了 */
+                       const size_t interface_num /* 发送数据报的索引 */
+) {
+    cerr << "DEBUG: adding route " << Address::from_ipv4_numeric(route_prefix).ip() << "/"
+         << static_cast<int>( prefix_length ) << " => " << (next_hop.has_value() ? next_hop->ip() : "(direct)")
+         << " on interface " << interface_num << "\n";
+    // route rule不存在，新增
+    route_t r = {route_prefix, prefix_length, next_hop, interface_num};
+    routing_table_.emplace_back(r);
 }
 
-void Router::route() {}
+void Router::route() {
+    // 遍历每一个网口
+    for (auto &ani: interfaces_) {
+        // 拿到每个网口的数据
+        while (auto dgram = ani.maybe_receive()) {
+            if (dgram) {
+                auto ip_datagram = dgram.value();
+                if (ip_datagram.header.ttl-- <= 0) continue;/*不转发ttl过期的ip数据报*/
+
+                // 找到next hop & interface id
+                uint8_t max_prefix_length = -1;
+                route_t ans_r;
+                for (route_t const r: routing_table_) {
+                    uint8_t const offset = static_cast<uint8_t>(32) - r.prefix_length;
+                    if ((r.route_prefix >> offset) == (ip_datagram.header.dst >> offset)) {/*前缀匹配*/
+                        if (max_prefix_length < r.prefix_length) {/*取最长前缀*/
+                            max_prefix_length = r.prefix_length;
+                            ans_r = r;
+                        }
+                    }
+                }
+
+                // 发送
+                ip_datagram.header.compute_checksum();/*重新计算校验和*/
+                AsyncNetworkInterface &out_interface = interface(ans_r.interface_id);
+                out_interface.send_datagram(ip_datagram, ans_r.next_hop.has_value() ? ans_r.next_hop.value(): Address::from_ipv4_numeric(ip_datagram.header.dst));
+            }
+        }
+    }
+}
